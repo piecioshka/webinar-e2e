0089fe8a6c1d5dbf61ad7cb9e7572f1c06ff2273
Piotr Kowalski <piecioshka@gmail.com>
2019-05-07 21:14:58 +0200

    Initialize project

---
+++ test/test.js
@@ -0,0 +1,32 @@
+module.exports = {};
+
+function test(name, cb) {
+    module.exports[name] = cb;
+}
+
+const PAGE_URL = 'https://piecioshka.pl/blog';
+const PAGE_LOAD_DELAY = 3000;
+
+async function setup(client) {
+    await client.url(PAGE_URL);
+    await client.waitForElementVisible('body', PAGE_LOAD_DELAY);
+}
+
+async function teardown(client) {
+    // ...
+}
+
+test('Spr. czy istnieje avatar', async (client) => {
+    await setup(client);
+    await client.assert.elementPresent('#me img');
+    // await teardown(client);
+});
+
+test('Spr. czy jest odpowiednia liczba elementów w menu', async (client) => {
+    await setup(client);
+    await client.elements('css selector', '.menu-item', (result) => {
+        const menuSize = result.value.length;
+        client.assert.equal(menuSize, 9);
+    });
+    // await teardown(client);
+});
