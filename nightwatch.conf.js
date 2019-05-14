043969e75711a8b5695746edea9a55a22a282ee2
Piotr Kowalski <piecioshka@gmail.com>
2019-05-14 18:39:40 +0200

    Install @types/mocha

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -13,7 +13,7 @@ module.exports = {
 
     "webdriver": {
         "start_process": true,
-        "server_path": 'node_modules/.bin/chromedriver',
+        "server_path": "node_modules/.bin/chromedriver",
         "port": 9515
     },
 
